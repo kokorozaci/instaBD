340045214e1bd2c7b2391d23bd7fd35d05b2850a
Mark Anna <anna.mark.spb@gmail.com>
2020-03-07 01:25:07 +0300

    1.0.2

--- global_mode.sql
+++ global_mode.sql
@@ -1,12 +1,3 @@
 
 SET @@global.sql_mode= 'STRICT_TRANS_TABLES';
 SET @@global.sql_mode= '';
-
-
-truncate table users_profiles;
-truncate table follows;
-delete from users where id = 1;
-delete from comments;
-
-truncate table instabd.comments_hashtags;
-truncate table instabd.medias_hashtags ;
